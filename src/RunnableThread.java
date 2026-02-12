31feabe8f7c3da31406a681d16b1f9b351590563
Merlin Mon Mathew <40564473+merlin2410@users.noreply.github.com>
2026-02-13 01:12:35 +0530

    Add MyRunnable class and main method structure
    Added MyRunnable class and RunnableThread main method with TODOs.

---
+++ src/RunnableThread.java
@@ -0,0 +1,12 @@
+class MyRunnable implements Runnable {
+    // TODO: Override run() method
+    // Loop from 1 to 5 and print numbers separated by space
+}
+
+public class RunnableThread {
+    public static void main(String[] args) {
+        // TODO: Create an instance of MyRunnable
+        // TODO: Create a Thread object passing the runnable instance
+        // TODO: Start the thread
+    }
+}
